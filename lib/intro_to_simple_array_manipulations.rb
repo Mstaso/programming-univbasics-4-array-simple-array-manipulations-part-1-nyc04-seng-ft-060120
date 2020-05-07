e86b35e730779251cb7f8ee5027249e94d6191a8
malcolm staso <mstaso1@gmail.com>
2020-05-07 15:45:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_push(colors_in_the_rainbow, violet)
   colors_in_the_rainbow.push(violet)
 end  
 
-def using_unshift
+def using_unshift(bouroughs_in_nyc, staten island)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
 end
